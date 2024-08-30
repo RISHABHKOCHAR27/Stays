526c187b7877f791ea6e77e1ed5179b5910e636c
rishabhkochar27 <rishabhkochar27@gmail.com>
2024-08-31 00:51:30 +0530

    Initial Commit

---
+++ Stays/StaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaysApp.swift
+//  Stays
+//
+//  Created by nuclei on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
